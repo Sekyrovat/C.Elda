1eba93235c55a86a5bac4f3a8f23cdfaadd933fd
Hector Hernandez Morales <hectorhm1596@gmail.com>
2018-11-19 15:53:02 -0600

    Types for VM
    VM with type validation for input

--- CEldaMaquinaVirtual.py
+++ CEldaMaquinaVirtual.py
@@ -12,7 +12,6 @@ with open('out.json', 'r') as json_file:
 	content = json_file.read()
 	data = json.loads(content, object_hook=hinted_tuple_hook)
 	json_file.close()
-print(data)
 
 class CEldaMaquinaVirtual(object):
 	"""docstring for CEldaMaquinaVirtual"""
@@ -23,19 +22,19 @@ class CEldaMaquinaVirtual(object):
 		self.currentCuad = 0;
 		self.memoriaGlobales = self.creaLista(self.frontera)
 		self.memoriaFuncion = self.creaLista(self.infoFunciones['main'][0])
-		print(len(self.memoriaFuncion))
 		self.memoriaTemporales = self.creaLista(self.infoFunciones['main'][1])
 		self.memACrear = []
+		self.dirRetorno = []
 		self.memoriaLlamada = []
 		self.pilaDePausaTemps = []
 		self.pilaDePausa= []
-		self.pilaRetorno = []
+		self.returnCuad =[]
 
 	def getValue(self, tripleta):
-		print('getValue')
-		print(tripleta)
+		#print('getValue')
+		#print(self.currentCuad)
+		#print(tripleta)
 		if tripleta[0] == 'v':
-			print(tripleta[1])
 			return tripleta[1]
 		elif tripleta[0] == 'd':
 			if tripleta[1] < self.frontera:
@@ -57,7 +56,7 @@ class CEldaMaquinaVirtual(object):
 				self.memoriaGlobales[tripleta[1]] = valor
 			else:
 				direccion = tripleta[1] - self.frontera
-				print(direccion)
+				#print(direccion)
 				self.memoriaFuncion[direccion] = valor
 		else:
 			self.memoriaTemporales[tripleta[1]] = valor
@@ -80,14 +79,11 @@ class CEldaMaquinaVirtual(object):
 	def asign(self, cuadruplo):
 		self.setValue(cuadruplo[3], cuadruplo[1][1])
 
-##########################
-
 	def opArit(self, cuadruplo):
-		print('opArit')
-		print(cuadruplo)
-		print(self.currentCuad)
+		# print('opArit')
+		# print(cuadruplo)
+		# print(self.currentCuad)
 		arg1 = self.getValue(cuadruplo[1])
-		print(arg1)
 		if cuadruplo[2] is None:
 			arg2 = arg1
 			arg1 = 0
@@ -97,8 +93,6 @@ class CEldaMaquinaVirtual(object):
 		if op == '+':
 			arg3 = arg1 + arg2
 		elif op == '-':
-			print(arg1)
-			print(arg2)
 			arg3 = arg1 - arg2
 		elif op == '*':
 			arg3 = arg1 * arg2
@@ -186,49 +180,62 @@ class CEldaMaquinaVirtual(object):
 			arg3 = arg1 == arg2 
 		self.setValue(cuadruplo[3], arg3)
 
-##########################
 	def ERA(self, cuadruplo):
 		self.pilaDePausa.append(self.memoriaFuncion)
 		self.pilaDePausaTemps.append(self.memoriaTemporales)
 		self.memACrear = self.infoFunciones[cuadruplo[1]]
 		self.memoriaLlamada = self.creaLista(self.memACrear[0])
 
-##########################
 	def GOSUB(self, cuadruplo):
+		self.returnCuad.append(self.currentCuad + 1)
 		self.currentCuad = cuadruplo[1] - 1
+		self.dirRetorno.append(cuadruplo[3])
 		self.memoriaFuncion = self.memoriaLlamada
 		self.memoriaTemporales = self.creaLista(self.memACrear[1])
 
-##########################
 	def PARAM(self, cuadruplo):
-		print(cuadruplo)
-		print(self.currentCuad)
+		# print(cuadruplo)
+		# print(self.currentCuad)
 		self.setParam(self.getValue(cuadruplo[1]), cuadruplo[3])
 
-	def ENDPROC(self):
-		pilaTemp = []
-		pilaTemp.append(self.pilaDePausa.pop())
-		self.memoriaFuncion = pilaTemp
-		pilaTemp = []
-		pilaTemp.append(self.pilaDePausaTemps.pop())
-		self.memoriaTemporales = pilaTemp
-
 ##########################
+
+	def ENDPROC(self):
+		self.memoriaFuncion = self.pilaDePausa.pop()
+		self.memoriaTemporales = self.pilaDePausaTemps.pop()
+		self.currentCuad = self.returnCuad.pop()
+
 	def RETURN(self, cuadruplo):
-		pilaTemp = []
-		pilaTemp.append(self.pilaDePausa.pop())
-		self.memoriaFuncion = pilaTemp
-		pilaTemp = []
-		pilaTemp.append(self.pilaDePausaTemps.pop())
-		self.memoriaTemporales = pilaTemp
-		self.pilaRetorno.append(self.getValue(cuadruplo[2]))
+		print(cuadruplo)
+		#print(self.pilaDePausaTemps)
+		valor = self.getValue(cuadruplo[1])
+		self.memoriaFuncion = self.pilaDePausa.pop()
+		self.memoriaTemporales = self.pilaDePausaTemps.pop()
+		self.setValue(self.dirRetorno.pop(), valor)
+		self.currentCuad = self.returnCuad.pop()
 
 	def WriteVar(self, cuadruplo):
 		print(self.getValue(cuadruplo[1]))
 
 	def ReadVar(self, cuadruplo):
 		temp = input()
-		print(setValue(cuadruplo[3], cuadruplo[1]))
+		tipo = cuadruplo[3][3]
+		if tipo == 'int':
+			temp = int(float(temp))
+		elif tipo == 'float'
+			temp = float(temp)
+		elif tipo == 'str'
+			temp = str(temp)
+		elif tipo == 'char'
+			temp = str(temp)[0]
+		elif tipo == 'bool'
+			if temp == 'TRUE' or temp == 'true' or temp >=1:
+				temp = 1
+			else:
+				temp = 0
+		self.setValue(cuadruplo[3][2], temp)
+			
+		# print(setValue(cuadruplo[3], cuadruplo[1]))
 
 	def creaLista(self, tam):
 		lista = [None] * tam
@@ -277,7 +284,7 @@ class CEldaMaquinaVirtual(object):
 				print(cuadruplo)
 			self.currentCuad = self.currentCuad + 1
 				# print(self.currentCuad)
-			print(cuadruplo)
+			#print(cuadruplo)
 
 if __name__ == '__main__':
 	maquina = CEldaMaquinaVirtual()
--- Ejemplo3.c
+++ Ejemplo3.c
@@ -8,20 +8,6 @@ see
 if
 multiline */
 
-const bool bMYTRUE = FALSE;
-const int iA = 13;
-const float fK = 25.0;
-const char cIDENTIFICADOR = '9';
-const char cPASA = 'p';
-const char cREPRUEBA = 'r';
-const string sMI_COMPI = "Es Hermoso";
-
-bool bContador;
-int iArrLoco[10];
-int iQueHaceAlgo;
-bool bMatMuyLoco[4][10];
-string sArrXYZ[iA];
-
 proto int factorialRecursivo(int iNumero1, int iNumero2);
 
 func int factorialRecursivo(int iNumero1, int iNumero2)
@@ -37,106 +23,27 @@ func int factorialRecursivo(int iNumero1, int iNumero2)
 
 func int fibonacci(int iNumeroFibonacci)
 {
-	if (iNumeroFibonacci == 0 || iNumeroFibonacci == 1)
+	if (iNumeroFibonacci < 2)
 	{
 		return 1;
 	}
 	return fibonacci(iNumeroFibonacci - 1) + fibonacci(iNumeroFibonacci - 2);
 }
 
+func int return3()
+{
+	return 3;
+}
+
 main
 {
-	bool bQueHaceAlgo;
 	int iParaSumar;
-	int iContJ;
-	char cDelSwitch;
-	int iArrEnorme[100];
-	cDelSwitch = 'p';
 
 	iParaSumar = 8;
 
-	switch (cDelSwitch)
-	{
-		case cPASA:
-			iParaSumar = 7;
-			break;
-		case cREPRUEBA:
-			iParaSumar = -7;
-			break;
-		case 't':
-		case 'y':
-		case 'z':
-			cDelSwitch = 'j';
-		default:
-			iParaSumar = 6;
-			break;
-	}
+	write(19);
+	write("some text");
+	write(return3());
+	write(fibonacci(5));
 
-	write(fibonacci(2));
-
-	bQueHaceAlgo = TRUE;
-	iQueHaceAlgo = 37 * 7 / 15 + 3 * 2 + iParaSumar << 2;
-	// Waza
-	iQueHaceAlgo = 37 * 7 / 15 + 3 * 2 + iParaSumar; //UwU
-	if (bQueHaceAlgo)
-	{
-		// esto hace cosas chidas
-		bQueHaceAlgo = FALSE;
-
-	} else if (5 - 3)
-	{
-		if (13 * 4)
-		{
-			iParaSumar = 5;
-		}
-		++iParaSumar;
-	} else if (0)
-	{
-		bQueHaceAlgo = TRUE;
-	} else //some comment
-	{
-		iParaSumar = 0 * 7;
-	}
-	iQueHaceAlgo = TRUE ? 15 + 15 ? 10 / 2.0 : 20 >> 2 : FALSE ? 30 : 40 * 12;
-	iArrLoco[7] = --iArrLoco[3];
-
-
-	for (iQueHaceAlgo = 0; iQueHaceAlgo < 100; ++iQueHaceAlgo)
-	{
-		for (iContJ = 0; iContJ < 200; ++iContJ)
-		{
-			iParaSumar += iQueHaceAlgo;
-		}
-		// some crazy stuff
-		for (iContJ = 0; iContJ < 300; ++iContJ)
-		{
-			for (iContJ = 0; iContJ < 600; ++iContJ)
-			{
-				if (iContJ % 3 == 0)
-				{
-					iParaSumar += iQueHaceAlgo;
-					break;
-				} else if (iContJ % 3 == 1)
-				{
-					for (iContJ = 20; iContJ < 200; iContJ += 20)
-					{
-						iParaSumar += iQueHaceAlgo;
-					}
-				} else
-				{
-					iParaSumar += iQueHaceAlgo;
-				}
-			}
-		}
-	}
-
-	while (iQueHaceAlgo)
-	{
-		--iQueHaceAlgo;
-	}
-
-	do
-	{
-		++iQueHaceAlgo;
-	} while (iQueHaceAlgo < 15);
 }
